6d9a8eae90d53098d35f94b80f49b8a4fb445bd6
Brady Klein <bmklein@mit.edu>
2024-01-22 09:39:09 +0100

    day 4 problem 3

---
+++ lec04-python-practice-2/problem3.py
@@ -0,0 +1,24 @@
+""" Grocery List Manager
+for the exercise, you will get 3 strings from the user
+and add them to a list to keep track of their groceries
+
+follow the following structure
+1)  initialize an empty list
+2)  get 3 items from user and add them
+3)  display the grocery list
+4)  ask the user for 1 item to remove
+5)  display the final list
+6)  bonus (optional) allow the user to continuously
+    add or remove items until the input the word 'done'
+"""
+
+# init the empty list
+
+# get 3 items from user and add them to the list
+# you'll want to use the .append() method
+
+# display the list
+
+# ask user for 1 item to remove
+
+# display the final list
